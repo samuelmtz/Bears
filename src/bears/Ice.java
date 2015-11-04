208af22ef4e7268ab02c7985d76359b9d9687c33
samuelmtz <s.martinezdelcampome@brentwood.ca>
2015-11-04 12:47:49 -0800

    A

---
+++ src/bears/Ice.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bears;
+
+import environment.Environment;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+
+/**
+ *
+ * @author samuelmartinezdelcampo
+ */
+class Ice extends Environment {
+
+    public Ice() {
+    }
+
+    @Override
+    public void initializeEnvironment() {
+    }
+
+    @Override
+    public void timerTaskHandler() {
+    }
+
+    @Override
+    public void keyPressedHandler(KeyEvent e) {
+    }
+
+    @Override
+    public void keyReleasedHandler(KeyEvent e) {
+    }
+
+    @Override
+    public void environmentMouseClicked(MouseEvent e) {
+    }
+
+    @Override
+    public void paintEnvironment(Graphics graphics) {
+    }
+    
+}
