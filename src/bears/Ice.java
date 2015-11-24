d0bb6e07eea80b18a4a4ac8eca2e65472d18e686
samuelmtz <s.martinezdelcampome@brentwood.ca>
2015-11-24 10:16:54 -0800

    better

--- src/bears/Ice.java
+++ src/bears/Ice.java
@@ -7,11 +7,14 @@ package bears;
 
 import environment.Environment;
 import grid.Grid;
+import images.ResourceTools;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.Point;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+import java.util.Random;
 
 /**
  *
@@ -20,32 +23,76 @@ import java.awt.event.MouseEvent;
 class Ice extends Environment {
 
     Grid grid;
+    
+    int ovalX;
+    int ovalY;
+    int slideX;
+    int slideXX;
+
+    Image bear;
+    Image seal;
+    Image iceberg;
+    
+  
+  
+    
+  
+    
+    
 
     public Ice() {
-        grid = new Grid(35, 25, 25, 25, new Point(10, 50), Color.PINK);
+        slideX = 900;
+        slideXX = 3400;
+        ovalX = 100;
+        ovalY= 100;
+        grid = new Grid(50, 25, 25, 25, new Point(10, 50), Color.PINK);
+        bear = ResourceTools.loadImageFromResource("bears/BEAR.gif");
+        seal = ResourceTools.loadImageFromResource("bears/SEAl.png");
+        iceberg = ResourceTools.loadImageFromResource("bears/iceberg.png");
     }
 
     @Override
     public void initializeEnvironment() {
+        background =ResourceTools.loadImageFromResource("Bears/ICE.png");
+        
+
     }
+    Image background;
     int counter;
 
     @Override
     public void timerTaskHandler() {
-//        System.out.println("Hey dude.." + ++counter);
+        slideX--;
+        slideX--;
+        slideX--;
+        slideX--;
+        slideXX--;
+        slideXX--;
+        slideXX--;        
+        slideXX--;
+
+
+
+        
     }
 
     @Override
     public void keyPressedHandler(KeyEvent e) {
 
         if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-            System.out.println("Go left!");
+//            System.out.println("Go left!");
+            ovalX= ovalX - 17;
         } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            System.out.println("Go right!");
+            ovalX= ovalX + 17;
+        }else if (e.getKeyCode() == KeyEvent.VK_RIGHT + KeyEvent.VK_UP) {
+            ovalX= ovalX + 17;
+            ovalY= ovalY -17;
+//            System.out.println("Go right!");
         } else if (e.getKeyCode() == KeyEvent.VK_UP) {
-            System.out.println("Go up!");
+            ovalY= ovalY - 17;
         } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-            System.out.println("Go down!");
+//            System.out.println("Go down!");
+            ovalY = ovalY +17;
         }
     }
 
@@ -61,19 +108,34 @@ class Ice extends Environment {
         }   if (e.getKeyCode() == KeyEvent.VK_D) {
             System.out.println("Key released - RIGHT");
         }
+        
     }
 
     @Override
     public void environmentMouseClicked(MouseEvent e) {
         System.out.println("Mouse clicked at " + e.getPoint());
         System.out.println("Mouse clicked in cell " + grid.getCellLocationFromSystemCoordinate(e.getPoint()));
+       
     }
 
     @Override
     public void paintEnvironment(Graphics graphics) {
+      graphics.drawImage(background, 0, 0, this);
         if (grid != null) {
             grid.paintComponent(graphics);
+       
+           
+            
         }
+        graphics.drawImage(bear, ovalX, ovalY, this);
+        graphics.drawImage(seal, slideX, 15, this);
+        graphics.drawImage(iceberg, slideX, 55, this);
+        graphics.drawImage(seal, slideXX ++, 20, this);
+        graphics.drawImage(iceberg, slideXX, 25, this);
+
+        
+
+
     }
 
 }
